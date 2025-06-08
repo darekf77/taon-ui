d207c56b3f4074b7522414ad70b79545e5b3c79f
Dariusz Filipiak <darekf77@gmail.com>
2025-06-08 20:07:18 +0200

    chore: update

--- src/lib/build-info._auto-generated_.ts
+++ src/lib/build-info._auto-generated_.ts
@@ -1,5 +1,6 @@
 // THIS FILE IS GENERATED - DO NOT MODIFY
 export const BUILD_FRAMEWORK_CLI_NAME = 'tnp';
+export const APP_ID = 'com.domain.example.taon-ui';
 export const PROJECT_NPM_NAME = 'taon-ui';
 export const CURRENT_PACKAGE_VERSION = '19.0.7';
 // THIS FILE IS GENERATED - DO NOT MODIFY
