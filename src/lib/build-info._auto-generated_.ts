33b2aef0ddd04b37092b6bf2187359946d3c2c26
Dariusz Filipiak <darekf77@gmail.com>
2025-08-03 21:35:05 +0200

    release: v19.0.17

--- src/lib/build-info._auto-generated_.ts
+++ src/lib/build-info._auto-generated_.ts
@@ -1,7 +1,8 @@
 // THIS FILE IS GENERATED - DO NOT MODIFY
 export const BUILD_FRAMEWORK_CLI_NAME = 'tnp';
 export const APP_ID = 'com.domain.example.taon-ui';
+export const BUILD_BASE_HREF = '';
 export const PROJECT_NPM_NAME = 'taon-ui';
-export const CURRENT_PACKAGE_VERSION = '19.0.16';
+export const CURRENT_PACKAGE_VERSION = '19.0.17';
 // THIS FILE IS GENERATED - DO NOT MODIFY
       
