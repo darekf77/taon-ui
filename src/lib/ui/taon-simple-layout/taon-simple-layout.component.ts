c780ef7002da27a947554b45045289afff089cf4
Dariusz Filipiak <darekf77@gmail.com>
2026-03-06 12:47:47 +0100

    fix: setting in modal + proper left right layout

--- src/lib/ui/taon-simple-layout/taon-simple-layout.component.ts
+++ src/lib/ui/taon-simple-layout/taon-simple-layout.component.ts
@@ -1,8 +1,14 @@
 //#region imports
 import { AsyncPipe, JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  input,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
@@ -29,6 +35,7 @@ import { TaonSimpleLayoutNavItem } from './taon-simple-layout.model';
     MatListModule,
     MatTabsModule,
     RouterModule,
+    MatDialogModule,
     JsonPipe,
     TaonThemeComponent,
   ],
@@ -36,9 +43,22 @@ import { TaonSimpleLayoutNavItem } from './taon-simple-layout.model';
 export class TaonSimpleLayoutComponent {
   protected router = inject(Router);
 
+  protected dialog = inject(MatDialog);
+
   navItems = input<TaonSimpleLayoutNavItem[]>();
 
   navigateTo(item: { path: string; label: string }): void {
     this.router.navigateByUrl(item.path);
+  }
+
+  openDialog(
+    enterAnimationDuration: string | number,
+    exitAnimationDuration: string | number,
+  ): void {
+    this.dialog.open(TaonThemeComponent, {
+      width: '400px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
   }
 }
--- src/lib/ui/taon-theme/taon-theme.component.ts
+++ src/lib/ui/taon-theme/taon-theme.component.ts
@@ -5,26 +5,11 @@ import { TaonThemeMode, TaonThemeService } from './taon-theme.service';
 
 @Component({
   selector: 'taon-theme',
-  standalone: true,
   imports: [MatSlideToggleModule],
-  template: `
-   <mat-slide-toggle
-      class="p-2"
-      [checked]="taonTheme.isAuto()"
-      (change)="taonTheme.toogleAuto($event.checked)">
-      Sync colors with system
-    </mat-slide-toggle>
-
-    <mat-slide-toggle
-      class="p-2"
-      [checked]="taonTheme.isDark()"
-      [disabled]="taonTheme.isAuto()"
-      (change)="taonTheme.setDark($event.checked)">
-      Dark mode
-    </mat-slide-toggle>
-  `,
+  templateUrl: './taon-theme.component.html',
 })
 export class TaonThemeComponent {
   TaonThemeMode = TaonThemeMode;
+
   taonTheme = inject(TaonThemeService);
 }
