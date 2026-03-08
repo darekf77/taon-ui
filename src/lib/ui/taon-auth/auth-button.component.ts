6b40172e9014f0f62562f5e6427cc13c90a2f950
Dariusz Filipiak <darekf77@gmail.com>
2026-03-08 02:03:04 +0100

    feat: allow to hide dashboard button from taon-auth component

--- src/lib/ui/taon-auth/auth-button.component.ts
+++ src/lib/ui/taon-auth/auth-button.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import {_} from 'tnp-core/src';
 
 import { AuthDialogComponent } from './auth-dialog.component';
 import { SessionService } from './session.service';
@@ -16,10 +17,12 @@ import { SessionService } from './session.service';
   templateUrl: './auth-button.component.html',
   styleUrl: './auth-button.component.scss',
 })
-export class AuthButtonComponent {
+export class AuthButtonComponent implements OnInit {
   @Input({ required: true }) linkToDashboard!: string;
 
   @Input({ required: true }) googleClientId!: string;
+
+  @Input() displayDashboardButton: boolean;
 
   readonly session = inject(SessionService);
 
@@ -42,5 +45,14 @@ export class AuthButtonComponent {
 
   logout() {
     this.session.logout();
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    // console.log('this.displayDashboardButton', this.displayDashboardButton);
+    this.displayDashboardButton = _.isBoolean(this.displayDashboardButton)
+      ? this.displayDashboardButton
+      : true;
   }
 }
