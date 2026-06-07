0ce201507b3d6c8a5d3d5538b912145342abb4fe
Dariusz Filipiak <darekf77@gmail.com>
2026-06-08 00:11:09 +0200

    feat: taon them close button

--- src/lib/ui/taon-theme/taon-theme.component.ts
+++ src/lib/ui/taon-theme/taon-theme.component.ts
@@ -3,15 +3,29 @@ import { Component, inject } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { TaonThemeMode, TaonThemeService } from './taon-theme.service';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 //#endregion
 
 @Component({
   selector: 'taon-theme',
-  imports: [MatSlideToggleModule],
+  imports: [
+    MatSlideToggleModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+  ],
   templateUrl: './taon-theme.component.html',
 })
 export class TaonThemeComponent {
   TaonThemeMode = TaonThemeMode;
+
+  private readonly dialogRef = inject(MatDialogRef<TaonThemeComponent>);
+
+  close(): void {
+    this.dialogRef.close();
+  }
 
   taonTheme = inject(TaonThemeService);
 }
